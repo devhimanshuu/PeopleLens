168c0dfa83bb8764ca8122351a30a9e5e8765b0a
devhimanshuu <devhimanshuu@gmail.com>
2026-08-09 13:31:03 +0530

    build: nestjs backend on aws lambda

---
+++ apps/api/src/lambda.ts
@@ -0,0 +1,53 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import configureServerlessExpress from '@codegenie/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
+import compression from 'compression';
+import express from 'express';
+import helmet from 'helmet';
+import { AppModule } from './app.module';
+import { API_GLOBAL_PREFIX } from './common/constants/app.constants';
+import { requestIdMiddleware } from './common/middleware/request-id.middleware';
+import { requestLoggerMiddleware } from './common/middleware/request-logger.middleware';
+
+let cachedServer: Handler;
+
+async function bootstrapServer(): Promise<Handler> {
+  const expressApp = express();
+  const adapter = new ExpressAdapter(expressApp);
+  const app = await NestFactory.create(AppModule, adapter);
+  const config = app.get(ConfigService);
+
+  app.use(helmet());
+  app.use(compression());
+  app.use(requestIdMiddleware());
+  app.use(requestLoggerMiddleware(config));
+
+  app.setGlobalPrefix(API_GLOBAL_PREFIX);
+  app.enableCors({
+    origin: config.get<string[]>('corsOrigins', ['http://localhost:3000']),
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
+  await app.init();
+
+  return configureServerlessExpress({ app: expressApp });
+}
+
+export const handler: Handler = async (event: unknown, context: Context, callback: Callback) => {
+  if (!cachedServer) {
+    cachedServer = await bootstrapServer();
+  }
+  return cachedServer(event, context, callback);
+};
